79bd544d60697d5ddbe06c85c5d2837da3a98388
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-04-26 15:09:20 +0530

    setting the priority to threads

--- src/MyThread.java
+++ src/MyThread.java
@@ -10,7 +10,14 @@ public class MyThread extends Thread{
         System.out.println(Thread.currentThread().getName());
     }
     public static void main(String[] args) {
-        MyThread t1 = new MyThread("Arjun");
-        t1.start();
+        MyThread tH = new MyThread("High Priority");
+        MyThread tM = new MyThread("Medium Priority");
+        MyThread tL = new MyThread("Low Priority");
+        tH.setPriority(Thread.MAX_PRIORITY); //max priority given for this
+        tM.setPriority(Thread.NORM_PRIORITY); //medium priority given for this
+        tL.setPriority(Thread.MIN_PRIORITY); //min priority given for this
+        tH.start();
+        tM.start();
+        tL.start();
     }
 }
