b3867779581d00589d4fe41897e624a5e0030776
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-05-01 21:26:31 +0530

    ReadWrite complete implementation

--- src/locks/ReadWriteCounter.java
+++ src/locks/ReadWriteCounter.java
@@ -24,7 +24,49 @@ public class ReadWriteCounter {
         }
     }
 
-    public static void main(String[] args) {
+    public int getCount(){
+        readLock.lock();
+        try{
+            return count;
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReadWriteCounter counter = new ReadWriteCounter();
+
+        Runnable readTask = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + ": reads :" + counter.getCount());
+                }
+            }
+        };
+        Runnable writeTask = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    counter.increment();
+                    System.out.println(Thread.currentThread().getName() + ": incremented");
+                }
+            }
+        };
+
+        // will create 3 thread, 1 write 2 read
+        Thread writerThread = new Thread(writeTask);
+        Thread readThread1 = new Thread(readTask);
+        Thread readThread2 = new Thread(readTask);
+
+        writerThread.start();
+        readThread1.start();
+        readThread2.start();
+
+        writerThread.join();
+        readThread1.join();
+        readThread2.join();
+
 
     }
 }
